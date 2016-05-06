8e828871df8aed23d967ff83c8ddc97753ea9dbf
Nicholas Marino <nac.marino@gmail.com>
2016-05-06 13:44:15 -0300

    optimized function

--- R functions/overdisp.R
+++ R functions/overdisp.R
@@ -1,5 +1,6 @@
 # function to calculate overdispersion
+# you just have to enter the object corresponding to the model ("model_name") and the function returns the overdisperion factor
 
-overdisp <- function(deviance, df.residual) {
-  deviance/df.residual
+overdisp <- function(model_name) {
+  model_name$deviance/model_name$df.residual
 }
