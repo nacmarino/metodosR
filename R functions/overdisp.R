f918099461e5f2d988e379902d0e5abe4e42d652
Nicholas Marino <nac.marino@gmail.com>
2016-05-09 21:57:36 -0300

    updated function for calculate overdisperion

--- R functions/overdisp.R
+++ R functions/overdisp.R
@@ -1,6 +1,17 @@
-# function to calculate overdispersion
-# you just have to enter the object corresponding to the model ("model_name") and the function returns the overdisperion factor
+# function to calculate overdispersion, from <http://glmm.wikidot.com/faq>
+# you just have to enter the object corresponding to the model ("model") and the function returns the overdisperion factor
 
-overdisp <- function(model_name) {
-  model_name$deviance/model_name$df.residual
+overdisp_fun <- function(model) {
+  ## number of variance parameters in 
+  ##   an n-by-n variance-covariance matrix
+  vpars <- function(m) {
+    nrow(m)*(nrow(m)+1)/2
+  }
+  model.df <- sum(sapply(VarCorr(model),vpars))+length(fixef(model))
+  rdf <- nrow(model.frame(model))-model.df
+  rp <- residuals(model,type="pearson")
+  Pearson.chisq <- sum(rp^2)
+  prat <- Pearson.chisq/rdf
+  pval <- pchisq(Pearson.chisq, df=rdf, lower.tail=FALSE)
+  c(chisq=Pearson.chisq,ratio=prat,rdf=rdf,p=pval)
 }
